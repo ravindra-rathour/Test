d513a104fe65d124da82f16f11cff072a94b80d4
Ravindra Rathour <ravi.rathour02@gmail.com>
2015-03-10 15:14:57 +0530

    set layout for home activity

--- app/src/main/java/com/example/ravi/test/HomeActivity.java
+++ app/src/main/java/com/example/ravi/test/HomeActivity.java
@@ -1,9 +1,15 @@
 package com.example.ravi.test;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 /**
  * Created by ravi on 10/3/15.
  */
 public class HomeActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
